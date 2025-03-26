6749f6cf04c6c2411620c8ceb0cc0861c6d0ca4f
Edy27D <deconescu.eduard@gmail.com>
2025-03-26 18:10:05 +0200

    Cateva tari

--- resources/js/Pages/Tour.tsx
+++ resources/js/Pages/Tour.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import WorldTourLayout from "@/Layouts/WorldTourLayout";
 import { Typography, Box, List, ListItem, ListItemText } from '@mui/material';
 import countries from "world-countries";
-import ReactCountryFlag from "react-country-flag"; 
+//import ReactCountryFlag from "react-country-flag"; 
+import ReactCountryFlag from "react-country-flag";
 import RecipeReviewCard from "@/Components/Card";
 
 interface Card { //TODO : SA IL PUN IN ALT FISIER CA STA DEGEABA AICI
@@ -26,6 +27,16 @@ const countryCards: CountryCards = { //un fel de implements din java interface
     "US": [
         { image: "/Images/Attractions/NY_liberty.jpg", title: "Statue of Liberty",description: "The Statue of Liberty in New York Harbor is a colossal neoclassical sculpture gifted by France, symbolizing freedom and democracy, and serving as an enduring icon of hope and opportunity for immigrants arriving in the United States." },
         { image: "/Images/Attractions/grand-canyon.jpg", title: "Grand Canyon",description: "The Grand Canyon in Arizona is a breathtaking natural wonder, carved by the Colorado River over millions of years, showcasing awe-inspiring layered rock formations and spanning over 277 miles of stunning, rugged landscapes." },
+    ],
+    "AR": [
+        {image:"/Images/Attractions/buenosAires.jpg", title: "Buenos Aires", description: "Buenos Aires is the vibrant capital city of Argentina, known for its European-style architecture, rich cultural heritage, and lively tango scene, offering a unique blend of history, art, and gastronomy." },
+        {image:"/Images/Attractions/cascada-iguazu.jpg", title: "Iguazu Falls", description: "Iguazu Falls is a majestic natural wonder located on the border of Argentina and Brazil, comprising a series of 275 cascading waterfalls, surrounded by lush rainforest, and recognized as one of the New Seven Wonders of Nature." },
+        {image:"/Images/Attractions/mendoza.jpg", title: "Mendoza", description: "Mendoza is a picturesque city in Argentina's wine country, renowned for its Malbec wines, stunning Andean landscapes, and outdoor adventures, including hiking, horseback riding, and whitewater rafting." }
+    ],
+    "PT":[
+      {image:"/Images/Attractions/SaoJorge.jpg", title: "Sao Jorge Castle", description: "Sao Jorge Castle is a historic fortress in Lisbon, Portugal, offering panoramic views of the city and the Tagus River, and serving as a symbol of the country's rich history and maritime heritage." },
+      {image:"/Images/Attractions/poduldomLuis.jpg", title: "Dom Luis I Bridge", description: "The Dom Luis I Bridge in Porto, Portugal, is an iconic double-deck metal bridge spanning the Douro River, connecting the city's historic Ribeira district with the Vila Nova de Gaia area, famous for its port wine cellars." },
+      {image:"/Images/Attractions/Evora_Tours_Portugal_Lisbon-1.jpg", title: "Evora", description: "Evora is a charming medieval town in Portugal's Alentejo region, known for its well-preserved historic center, Roman ruins, and the iconic Chapel of Bones, a macabre chapel adorned with human skulls and bones." }
     ],
     // aici mai pot adauga
 };
