40ce1b37426c7aff70e41331a14b96d6a06d6fc8
JJTecca <maiorcristian123@gmail.com>
2025-05-28 14:14:55 +0300

    resolve

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -13,8 +13,8 @@ class UserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        //return true; 
-        return Auth::check();
+        return true; 
+        //return Auth::check();
     }
 
     /**
