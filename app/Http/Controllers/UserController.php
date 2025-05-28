533f3b29326ac811038456869dc1bb86000e53bc
JJTecca <maiorcristian123@gmail.com>
2025-05-28 14:21:30 +0300

    redirect dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,6 @@ class UserController extends Controller
         ]);
         Auth::login($user);
         
-        return back()->withErrors(['email' => 'Authentication failed.']);
+        return redirect()->route('dashboard');
     }
 }
