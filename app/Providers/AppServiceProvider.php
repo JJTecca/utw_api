1394cf864f7c8aebebf12eb15e5fffe26774ccd2
JJTecca <your_email@example.com~>
2025-11-15 17:59:31 +0200

    [REFACTOR] Back to local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
-use Illuminate\Support\Facades\URL;  // Add this
+//use Illuminate\Support\Facades\URL;  // Add this
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,10 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS in production
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        // PRODUCTION SETUP : Force HTTPS
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
 
         Vite::prefetch(concurrency: 3);
     }
--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    laravel({
-      input: 'resources/js/app.tsx',
-      ssr: 'resources/js/ssr.tsx',
-      refresh: true,
-    }),
-    react(),
-  ],
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-  base: '/',
-  server: {
-    hmr: {
-      //protocol: 'wss' //wss pentru https
-      protocol: 'ws', // use ws for local HTTP
-      host: '127.0.0.1', // optional, ensures IPv4
+        laravel({
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    build: {
+      chunkSizeWarningLimit: 1000, // am limitat la 1000
     }
-  }
+    //PRODUCTION : Update to wss protocol
+  // base: '/',
+  // server: {
+  //   hmr: {
+  //     protocol: 'wss'
+  //   }
+  // }
 });
