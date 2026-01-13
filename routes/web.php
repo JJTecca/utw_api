be1460a16635910a9f634921e99d4e173411f99b
Danny-AS <carteafarsora55@gmail.com>
2026-01-13 20:55:02 +0200

    [FEATURE] Prevent XSS + Throttle for brute force

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'throttle:60,1')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
