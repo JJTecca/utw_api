b7f6ef40fe8058a6c1099831f074726d05900c1f
JJTecca <maiorcristian123@gmail.com>
2026-01-14 03:38:08 +0200

    [HOTFIX] Remove throttle

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::middleware('auth', 'throttle:60,1')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
